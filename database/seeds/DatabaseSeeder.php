5ace213b3a13cb73256e12d8742917e8ff91120b
Ryan B. Abcede <ryan@syntactics.local>
2017-12-14 22:02:57 +0800

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,9 +18,31 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('RDYb1&zXuzDk8MvKRk'),
         ]);
 
+        DB::table('users')->insert([
+            'name' => 'Student User',
+            'email' => 'student@mail.com',
+            'admin' => '0',
+            'password' => bcrypt('RDYb1&zXuzDk8MvKRk'),
+        ]);
+
         DB::table('subjects')->insert([
             'subject' => 'English',
             'user_id' => '1',
+        ]);
+
+        DB::table('subjects')->insert([
+            'subject' => 'Math',
+            'user_id' => '1',
+        ]);
+
+        DB::table('subjects')->insert([
+            'subject' => 'Science',
+            'user_id' => '1',
+        ]);
+
+        DB::table('subjects')->insert([
+            'subject' => 'History',
+            'user_id' => '2',
         ]);
 
         DB::table('questions')->insert([
@@ -28,12 +50,29 @@ class DatabaseSeeder extends Seeder
             'question' => 'What is the plural for fish?',
         ]);
 
+        DB::table('questions')->insert([
+            'subject_id' => '2',
+            'question' => '1+1?',
+        ]);
+
         DB::table('options')->insert([
             'question_id' => '1',
-            'option_a' => 'shark',
-            'option_b' => 'dragon',
-            'option_c' => 'fishes',
-            'option_d' => 'schwarzenegger',
+            'option' => 'shark',
+        ]);
+
+        DB::table('options')->insert([
+            'question_id' => '1',
+            'option' => 'dragon',
+        ]);
+
+        DB::table('options')->insert([
+            'question_id' => '1',
+            'option' => 'fishes',
+        ]);
+
+        DB::table('options')->insert([
+            'question_id' => '1',
+            'option' => 'schwarzenegger',
         ]);
     }
 }
