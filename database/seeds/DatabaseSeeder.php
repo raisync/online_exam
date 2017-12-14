fa1b5b8ac53afe4fb3ba8805254a755edb78d96a
Ryan <ryan@syntacticsinc.com>
2017-12-14 08:40:28 +0800

    seeder fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin@mail.com',
+            'admin' => '1',
+            'password' => bcrypt('RDYb1&zXuzDk8MvKRk'),
+        ]);
+
+        DB::table('subjects')->insert([
+            'subject' => 'English',
+            'user_id' => '1',
+        ]);
     }
 }
